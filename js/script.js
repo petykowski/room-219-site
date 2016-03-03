548d19e3add128a2fedd3e023f2e7a80e2e7c077
Sean Petykowski <spetykowski@gmail.com>
2016-03-03 10:21:07 -0500

    Moved scripts to external files.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(window).load(function() {
+    
+    $( "div.lowtemp" ).click(function() {
+        $( "div.subtext-low" ).delay(100).fadeIn(100);
+        $( "div.subtext-avg" ).hide();
+        $( "div.subtext-high" ).hide();
+    });
+
+    $( "div.avgtemp" ).click(function() {
+        $( "div.subtext-low" ).hide();
+        $( "div.subtext-avg" ).delay(100).fadeIn(100);
+        $( "div.subtext-high" ).hide();
+    });
+
+    $( "div.hightemp" ).click(function() {
+        $( "div.subtext-low" ).hide();
+        $( "div.subtext-avg" ).hide();
+        $( "div.subtext-high" ).delay(100).fadeIn(100);
+    });
+
+    $( "div.currenttemp" ).click(function() {
+        $( "div.subtext-low" ).hide();
+        $( "div.subtext-avg" ).hide();
+        $( "div.subtext-high" ).hide();
+    });
+    
+});
