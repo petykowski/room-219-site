f7486360ca2ca4e2e7d7c749d9da51a4ee5bb2ac
Sean Petykowski <spetykowski@gmail.com>
2016-03-03 16:00:49 -0500

    MySQL request moved to external php file.

--- index.php
+++ index.php
@@ -15,37 +15,8 @@
     <body>
         
 <?php
-// Access provided by database configuration file
-require_once 'database/config.php';
-
-// Create connection to MySQL Database
-$conn = new mysqli($servername, $username, $password, $dbname);
-    
-// Check for sucessful connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-        
-// Request most recent recorded temperature
-$res = $conn->query("SELECT * FROM env_sensors ORDER BY TempID DESC LIMIT 1");
-
-// Request lowest temperature of the past 7 days  
-$lowtempreq = $conn->query("SELECT TemperatureF AS low_temp FROM env_sensors WHERE Date_Time BETWEEN (CURRENT_TIMESTAMP() - INTERVAL 7 DAY) AND CURRENT_TIMESTAMP() ORDER BY TemperatureF LIMIT 1");
-$lowtemp = $lowtempreq->fetch_row();
-
-// Request average temperature of the past 7 days       
-$avg = $conn->query("SELECT AVG(TemperatureF) AS avg_temp FROM env_sensors WHERE Date_Time BETWEEN (CURRENT_TIMESTAMP() - INTERVAL 7 DAY) AND CURRENT_TIMESTAMP()");
-$avgrow = $avg->fetch_row();
-
-// Request highest temperature of the past 7 days  
-$hightempreq = $conn->query("SELECT TemperatureF AS high_temp FROM env_sensors WHERE Date_Time BETWEEN (CURRENT_TIMESTAMP() - INTERVAL 7 DAY) AND CURRENT_TIMESTAMP() ORDER BY TemperatureF DESC LIMIT 1");
-$hightemp = $hightempreq->fetch_row();
-
-// Request date and time of most recent recorded temperature
-$currentDateReq = $conn->query("SELECT Date_Time FROM env_sensors ORDER BY TempID DESC LIMIT 1");
-$currentDate = $currentDateReq ->fetch_row();
-date_default_timezone_set('America/New_York');
-        
+// Request data from MySQL Server via request.php
+require_once 'database/request.php';
 ?>
   
         <div class="currenttemp">
