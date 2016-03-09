193e1d9fe0ce5c5cf7caf32da1da40e96925a95d
Sean Petykowski <spetykowski@gmail.com>
2016-03-08 23:05:47 -0500

    Added initial files for Daily Chart.

---
+++ database/getDataDaily.php
@@ -0,0 +1,67 @@
+<?php
+
+// Access provided by database configuration file
+require_once 'config.php';
+
+// Create connection to MySQL Database
+$con = new mysqli($servername, $username, $password, $dbname);
+
+// Check for sucessful connection
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+// Create request for temperature reading at the top of each hour
+$hourlyrequest = ("SELECT TemperatureF AS Temperature, DATE_FORMAT(Date_Time, '%l:%i %p') AS Time
+FROM env_sensors
+WHERE Date_Time IN (
+	SELECT MIN(Date_Time)
+	FROM env_sensors
+	WHERE DATE(Date_Time) = CURDATE()
+	GROUP BY HOUR(Date_Time)
+)");
+
+// Query the request
+$result = mysqli_query($con,$hourlyrequest);
+
+// Close MySQL connection
+mysqli_close($con);
+
+// Establish JSON heading
+$table = array();
+$table['cols'] = array(
+    
+    //Labels for the chart, these represent the column titles
+    array('id' => '', 'label' => 'Time', 'type' => 'string'),
+    array('id' => '', 'label' => 'Temperature (F)', 'type' => 'number')
+);
+
+// Prepare for ForEach loop
+$rows = array();
+
+// Parse through results
+foreach($result as $row){
+    $temp = array();
+    
+    // Add date and temperature to temp arrays then to rows array
+    $temp[] = array('v' => (string) $row['Time']);
+    $temp[] = array('v' => (float) $row['Temperature']); 
+    $rows[] = array('c' => $temp);
+    
+}
+
+// Free up memory
+$result->free();
+
+// Build table and encode
+$table['rows'] = $rows;
+$jsonTable = json_encode($table, true);
+
+// Provide JSON
+echo $jsonTable;
+
+//echo '<pre>';
+//echo json_encode($table, JSON_PRETTY_PRINT);
+//echo '</pre>';
+
+?>
---
+++ js/chart-by-the-day.js
@@ -0,0 +1,53 @@
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var jsonData = $.ajax({
+      url: "database/getDataHourly.php",
+      dataType:"json",
+      async: false
+}).responseText;
+
+// Create our data table out of JSON data loaded from server.
+var data = new google.visualization.DataTable(jsonData);
+
+var showEvery = parseInt(data.getNumberOfRows() / 4);
+    
+var options = {
+    chartArea: { left: '10%', width: "88%" },
+    legend: {
+        position: 'top',
+        alignment: 'end',
+    },
+    backgroundColor: 'black',
+    hAxis: {
+        baseline: '100',
+        showTextEvery: showEvery,
+        maxTextLines: 1,
+        textStyle: {
+            color: 'white',
+        },
+    },
+    vAxis: {
+        textStyle: {
+            color: 'white',
+        },
+    },
+    lineWidth: 1.5,
+    legend: {
+        position: 'bottom',
+        textStyle: {
+            color: 'white',
+        },
+    },
+    titleTextStyle: {
+        color: 'white',
+        fontSize: 12,
+    },
+};
+
+var chart = new google.visualization.LineChart(document.getElementById('chart-hourly'));
+
+chart.draw(data, options);
+    
+}
